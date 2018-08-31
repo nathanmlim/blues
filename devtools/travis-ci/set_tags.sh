8079991f64c8001651d2af65f98f70611cfc6877
Nathan M. Lim <nathanmlim@gmail.com>
2018-08-31 16:28:09 -0700

    update tags

--- devtools/travis-ci/set_tags.sh
+++ devtools/travis-ci/set_tags.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 export TRAVIS_BRANCH="${BRANCH}"
+IFS="- " read -r -a GIT <<< `git describe --tags`
 if [ ${RELEASE} = "true" ]; then
     echo "Deploying RELEASE build" ;
-    git fetch --tags --all
-    git tag -d ${TAG}
-    git push origin :refs/tags/${TAG} ;
-    git tag -f ${TAG} ;
-    git push origin ${BRANCH} --tags ;
+    # git fetch --tags --all
+    # git tag -d ${TAG}
+    # git push origin :refs/tags/${TAG} ;
+    # git tag -f ${TAG} ;
+    # git push origin ${BRANCH} --tags ;
     export LABEL="main" ;
     export TRAVIS_TAG=${GIT[0]} ;
 else
